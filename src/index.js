9879d4464170d742782c16d38358586ab5e2f02f
avisionx <avi.grg4567@gmail.com>
2020-09-12 16:50:21 +0530

    add build flow for ci with basic npm modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './custom.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
