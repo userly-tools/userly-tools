524b357c6acfd2ff64e63fcfb3a8e1efb6d29849
avisionx <avi.grg4567@gmail.com>
2020-09-12 16:58:28 +0530

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {Button} from 'reactstrap';
+import { HashRouter as DefaultRouter, Switch, Route } from "react-router-dom";
 
-function App() {
+var createBrowserHistory = require("history").createBrowserHistory;
+const history = createBrowserHistory();
+
+const App = () => {
   return (
-    <Button color="primary">Hello World!</Button>
+    <DefaultRouter history={history}>
+      Header
+      <Switch>
+        <Route exact path="/" component={() => {return <>Home</>}} />
+        <Route exact path="/login" component={() => {return <>Login</>}} />
+      </Switch>
+      Footer
+    </DefaultRouter>
   );
 }
 
